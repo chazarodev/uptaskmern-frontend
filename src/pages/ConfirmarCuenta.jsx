7eb2b29b9c5f84c3bebe41f38b5d2ad6fb71a009
Alberto Cházaro <shazarerom@gmail.com>
2022-09-16 14:47:24 -0500

    Confirmando la cuenta via token

--- src/pages/ConfirmarCuenta.jsx
+++ src/pages/ConfirmarCuenta.jsx
@@ -1,6 +1,42 @@
-
+import { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+import axios from "axios"
+import Alerta from "../components/Alerta"
 
 const ConfirmarCuenta = () => {
+
+  const [alerta, setAlerta] = useState({})
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false)
+
+  const params = useParams()
+
+  const { id } = params
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+        const url = `${import.meta.env.VITE_BACKEND_URL}/api/usuarios/confirmar/${id}`
+        const { data } = await axios(url)
+        
+        setAlerta({
+          msg: data.msg,
+          error: false,
+        })
+
+        setCuentaConfirmada(true)
+
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true
+        })
+      }
+    }
+    confirmarCuenta()
+  }, [])
+  
+  const { msg } = alerta
+
   return (
     <>
     <h1 className="text-sky-600 font-black text-4xl capitalize">
@@ -9,6 +45,14 @@ const ConfirmarCuenta = () => {
       Cuenta
     </span>
     </h1>
+    <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+      {msg && <Alerta alerta={alerta} />}
+      {cuentaConfirmada && (
+        <Link to={'/'} className="block text-center my-5 text-slate-500 uppercase text-sm">
+          Cuenta confirmada, Inicia Sesión aquí.
+        </Link>
+      )}
+    </div>
     </>
   )
 }
