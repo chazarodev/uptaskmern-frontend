7ce8c50cbcfb76763baee7a62513bcfd4ef5e1e4
Alberto Cházaro <shazarerom@gmail.com>
2022-09-16 16:36:07 -0500

    Validando la autenticación, guardar JWT en local storage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,38 @@
-import { Link } from "react-router-dom";
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import Alerta from "../components/Alerta"
+import{ clienteAxios } from "../config/clienteAxios"
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [alerta, setAlerta] = useState({})
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+
+        if ([email, password].includes('')) {
+            setAlerta({
+                msg: "Todos los campos son obligatorios",
+                error: true,
+            })
+        }
+
+        try {
+            const { data } = await clienteAxios.post('usuarios/login', {email, password})
+            //Almacenar JWT en localStorage
+            localStorage.setItem('token', data.token)
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true,
+            })
+        }
+    }
+
+    const { msg } = alerta
+
   return (
     <>
         <h1 className="text-sky-600 font-black text-4xl capitalize">
@@ -9,7 +41,11 @@ const Login = () => {
                 proyectos
             </span>
         </h1>
-        <form className="my-10 bg-white shadow rounded-lg p-10">
+        {msg && <Alerta alerta={alerta} />}
+        <form 
+            className="my-10 bg-white shadow rounded-lg p-10"
+            onSubmit={handleSubmit}    
+        >
             <div className="my-5">
                 <label 
                     htmlFor="email" 
@@ -22,13 +58,15 @@ const Login = () => {
                     type={'email'}
                     placeholder={'Email de Registro'}
                     className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
-                />
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    />
             </div>
             <div className="my-5">
                 <label 
                     htmlFor="password" 
                     className="uppercase text-gray-600 block text-xl font-bold"
-                >
+                    >
                     Password
                 </label>
                 <input 
@@ -36,7 +74,9 @@ const Login = () => {
                     type={'password'}
                     placeholder={'Escribe tu password'}
                     className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
-                />
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    />
             </div>
             <input 
                 type="submit" 
