850f05e0117b61b860b655db0eb2cc4ce27d0b8d
Alberto Cházaro <shazarerom@gmail.com>
2022-09-16 15:58:33 -0500

    Solicitando el email al usuario para comunicarnos con el backend

--- src/pages/OlvidePassword.jsx
+++ src/pages/OlvidePassword.jsx
@@ -1,6 +1,46 @@
-import { Link } from "react-router-dom";
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import Alerta from "../components/Alerta"
+import axios from "axios"
 
 const OlvidePassword = () => {
+
+  const [email, setEmail] = useState('')
+  const [alerta, setAlerta] = useState({})
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    if (email === '' || email.length < 6) {
+      setAlerta({
+        msg: "Debes colocar un email",
+        error: true,
+      })
+      return
+    }
+
+    setAlerta({})
+
+    try {
+      const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/usuarios/olvide-password`, {email})
+
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      })
+
+
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      })
+    }
+
+  }
+
+  const { msg } = alerta
+
   return (
     <>
     <h1 className="text-sky-600 font-black text-4xl capitalize">
@@ -9,7 +49,11 @@ const OlvidePassword = () => {
         proyectos
     </span>
     </h1>
-    <form className="my-10 bg-white shadow rounded-lg p-10">
+    {msg && <Alerta alerta={alerta} />}
+    <form 
+      className="my-10 bg-white shadow rounded-lg p-10"
+      onSubmit={handleSubmit}  
+    >
       <div className="my-5">
           <label 
               htmlFor="email" 
@@ -22,6 +66,8 @@ const OlvidePassword = () => {
               type={'email'}
               placeholder={'Email de Registro'}
               className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
+              value={email}
+              onChange={ e => setEmail(e.target.value)}
           />
       </div>
       <input 
