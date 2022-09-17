52955d81120d6aaa960e78ee1050a3956fc767cd
Alberto Cházaro <shazarerom@gmail.com>
2022-09-16 20:08:52 -0500

    Creando el context de autenticacion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,15 @@ import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import Alerta from "../components/Alerta"
 import{ clienteAxios } from "../config/clienteAxios"
+import useAuth from "../hooks/useAuth"
 
 const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [alerta, setAlerta] = useState({})
+
+    const { setAuth } = useAuth()
 
     const handleSubmit = async e => {
         e.preventDefault()
@@ -23,6 +26,7 @@ const Login = () => {
             const { data } = await clienteAxios.post('usuarios/login', {email, password})
             //Almacenar JWT en localStorage
             localStorage.setItem('token', data.token)
+            setAuth(data)
         } catch (error) {
             setAlerta({
                 msg: error.response.data.msg,
