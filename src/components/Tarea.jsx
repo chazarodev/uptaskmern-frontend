3a457c59bb55033933e91af8691eb7b69a94b1ef
Alberto Cházaro <shazarerom@gmail.com>
2022-10-10 11:01:03 -0500

    Corrigiendo diseño responsive

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -27,7 +27,7 @@ const Tarea = ({tarea}) => {
             </p>
             {estado && <p className="text-xs bg-green-600 uppercase p-1 rounded-lg text-white">Completado por: {tarea.completado.nombre}</p>}
         </div>
-        <div className="flex gap-2">
+        <div className="flex flex-col lg:flex-row gap-2">
             {admin && (
                 <button
                     className="bg-indigo-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
